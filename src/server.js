bb5c0b5b80da7e9fd00a97acff5b4ac9651611ba
jayel888 <15644@coderacademy.edu.au>
2025-01-23 20:55:09 +1100

    feat: added default get route

--- src/server.js
+++ src/server.js
@@ -12,6 +12,15 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
+// app.verb(path, callback);
+app.get("/", (request, response) => {
+    // response.send("<h1>Hello World!</h1>");
+
+    response.json({
+        message: "Hello World!"
+    });
+});
+
 app.use("/api/auth", authRoutes)
 
 app.use("/api/posts", postRoutes)
